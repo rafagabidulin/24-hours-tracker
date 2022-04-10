4ae2587789fa8cd94ee92534fcd4a16d21a7fa21
rafagabidulin <rafagabidulin@gmail.com>
2022-04-10 23:51:13 +0300

    created Chart

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-
+import "@progress/kendo-theme-material/dist/all.css";
+import "hammerjs";
+import "./App.css";
+import Donut from "./components/Chart.js";
+ 
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>24HoursTracker</h1>
+        <div className="section">
+          <Donut />
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,66 @@
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import {
+  Chart,
+  ChartTitle,
+  ChartLegend,
+  ChartSeries,
+  ChartSeriesItem,
+  ChartSeriesLabels,
+} from "@progress/kendo-react-charts";
+import { COLORS } from "./constants.js";
 
-const Chart = 
+// Graph data
+const applicationsStatusThisMonth = [
+  {
+    status: "Работа",
+    value: 29,
+    color: COLORS.accepted,
+  },
+  {
+    status: "Ужин",
+    value: 14,
+    color: COLORS.interviewing,
+  },
+  {
+    status: "Дорога домой",
+    value: 14,
+    color: COLORS.rejected,
+  },
+  {
+    status: "Бег",
+    value: 14,
+    color: COLORS.accepted,
+  },
+  {
+    status: "Сон",
+    value: 29,
+    color: COLORS.pending,
+  },
+];
+ 
+// Show category label for each item in the donut graph
+const labelContent = e => e.category;
+ 
+const Charts = props => {
+  return (
+    <Chart>
+      <ChartTitle text="11 апреля" />
+      <ChartLegend visible={false} />
+      <ChartSeries>
+        <ChartSeriesItem
+          type="donut"
+          data={applicationsStatusThisMonth}
+          categoryField="status"
+          field="value"
+        >
+          <ChartSeriesLabels
+            color="#fff"
+            background="none"
+            content={labelContent}
+          />
+        </ChartSeriesItem>
+      </ChartSeries>
+    </Chart>
+  );
+};
+ 
+export default Charts;
